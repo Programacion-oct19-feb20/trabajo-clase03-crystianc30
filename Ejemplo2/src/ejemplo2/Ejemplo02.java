b5ad0b888e7e817a21a7e39cb0daf27d0a70395e
crystianc30 <crystian_carrion8@hotmail.com>
2019-10-17 12:46:39 -0500

    ejemplos

---
+++ Ejemplo2/src/ejemplo2/Ejemplo02.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Ejemplo02 {
+
+    public static void main(String[] args) {
+        String nombres = "CrystianAndres";
+        System.out.println(nombres);
+
+        String apellidos = "CarrionMartinez";
+        System.out.println(apellidos);
+
+        int edad;
+        edad = 18;
+        System.out.println(edad);
+
+        String universidad = "Utpl";
+        System.out.println(universidad);
+
+    }
+}
